61054748afcc6c801d117f38e788962e6140a392
Chanmuzi <101971295+Chanmuzi@users.noreply.github.com>
2022-09-16 01:43:34 +0900

    2차원 리스트 뒤집기

--- 2차원 리스트 뒤집기_나의 풀이.py
+++ 2차원 리스트 뒤집기_나의 풀이.py
@@ -5,6 +5,7 @@ def solution(mylist):
     for i in range(length):
         # 2차원 배열 만들기
         answer.append([])
+        # 길이만큼 2중 for문 만들기
         for j in range(length):
             # 행과 열을 반대로 추가
             answer[i].append(mylist[j][i])
